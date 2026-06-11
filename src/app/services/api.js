905977188b780e3b41b83e3db84c15f3afe6b153
SAULALEE <saulalejandro678@gmail.com>
2026-06-10 22:16:14 -0600

    chore: configure vite proxy and centralize axios api client

---
+++ src/app/services/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: '/api/v1',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Request interceptor to automatically inject the JWT Bearer Token if present
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('katedra_token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
